79d1c2156079a76c506ed8723a82ab6a27f02ef6
Tyson Gushiken <tysonism@gmail.com>
2018-05-18 10:32:00 -0700

    Incorportate tooltip on astronaut image for stretch task

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ $(function () {
   const $contactForm = $('#contactForm');
   const $contactSuccessModal = $('#contactSuccessModal');
 
+  // Enable tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+
   // Trigger modal on submit, prevent post
   $contactForm.submit(function(e) {
     e.preventDefault(e);
@@ -13,5 +16,5 @@ $(function () {
   $contactSuccessModal.on('hidden.bs.modal', function (e) {
     $contactForm[0].reset();
   })
-  
+
 });
