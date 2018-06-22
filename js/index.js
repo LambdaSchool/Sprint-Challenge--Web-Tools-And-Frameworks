392e5ff4fa73e68d7f70a2d3332e3e73f1f343e0
Ashwin <aswinjeganath@gmail.com>
2018-06-23 01:39:35 +0530

    Add fade in on scroll

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 $(function() {
   $('[data-toggle="tooltip"').tooltip();
+
+  $('.scroll-fade').css('opacity', 0);
+
+  $(window).scroll(function() {
+    $('.scroll-fade').each(function() {
+      let objectBottom = $(this).position().top + $(this).outerHeight();
+      let windowBottom = $(window).scrollTop() + $(window).height();
+
+      if (windowBottom > objectBottom) {
+        $(this).fadeTo('slow', 1);
+      }
+    });
+  });
 });
