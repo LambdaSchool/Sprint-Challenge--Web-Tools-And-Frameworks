f2bde7cf087dd1252d18995d7f4bfb7862a14b83
Lauren <llwor94@gmail.com>
2018-06-22 10:29:41 -0700

    added navbar, scrollspy and accordian

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const offset = 130;
+$(".navbar li a").click(function(event) {
+      event.preventDefault();
+      $($(this).attr("href"))[0].scrollIntoView();
+      scrollBy(0, -offset);
+    });
