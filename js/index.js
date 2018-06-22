b41564dac13cdb0dddccf5532a624d310e01e142
Lauren <llwor94@gmail.com>
2018-06-22 12:36:03 -0700

    updated font colors. added verification to sign in modal. added javascriptmodal  dismissal if all forms are valid

--- js/index.js
+++ js/index.js
@@ -4,3 +4,22 @@ $(".navbar li a").click(function(event) {
       $($(this).attr("href"))[0].scrollIntoView();
       scrollBy(0, -offset);
     });
+
+    (function() {
+      'use strict';
+      window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+            $('#myModal').modal('hide');
+          }, false);
+        });
+      }, false);
+    })();
