22cd61f07fc243eac0d0a316b066994483be0d90
chinopuente <joseph.bozzi16@gmail.com>
2018-07-15 16:12:32 -0400

    Anwsers complete

---
+++ challenge.js
@@ -0,0 +1,13 @@
+
+console.log('\n********** Exercise 3 of 4 **********');
+const adjectiveArray = [ 'awesome', 'fantastic', 'amazing', 'wonderful', 'fabulous', 'incredible', 'marvelous' ];
+// Using both nameArray and adjectiveArray, make a
+// "for" loop that console.log()'s a sentence for each
+// corresponding value in the arrays. Use the string "is"
+// to combine the name with the adjective. For example:
+// "Jacquelynn is awesome"
+// "Csaba is fantastic" et cetera...
+
+adjectiveArray.forEach(function(item, index, array){
+    console.log(item, index);
+});
